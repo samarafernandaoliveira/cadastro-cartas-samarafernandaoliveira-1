0d22e17e448fdcaa6757ee1c82eab30c1b2d2189
samarafernandaoliveira <samarafernandaoliveira03@gmail.com>
2026-04-20 13:47:04 +0000

    Fiz o desafio nível aventureiro

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -7,56 +7,92 @@
 int main() {
  
   // Área para definição das variáveis para armazenar as propriedades das cidades
-char estado1 [14] = "Bahia";
-char codigo1 [10] = "B01";
-char nomedacidade1 [50] = "Salvador";
-int populacao1 = 4063170;
-float area1 = 693.44;
-float pib1 = 31724.15;
-int pontoturistico1 = 30;
+char estado1 [14];
+char codigo1 [10];
+char nomedacidade1 [50];
+int populacao1;
+float area1;
+float pib1;
+int pontoturistico1;
+float densidadepopulacional1;
+float pibpercapita1;
 
-char estado2 [14] = "Pernambuco";
-char codigo2 [10] = "B02";
-char nomedacidade2 [50] = "Recife";
-int populacao2 = 1488920;
-float area2 = 218.843;
-float pib2 = 54970305;
-int pontoturisco2 = 30;
+
+char estado2 [14];
+char codigo2 [10];
+char nomedacidade2 [50];
+int populacao2;
+float area2;
+float pib2;
+int pontoturistico2;
+float densidadepopulacional2;
+float pibpercapita2;
+
 
 
   // Área para entrada de dados
 printf ("Carta 1: \n");
 
-printf ("Estado: %s \n", estado1); // %s é o formato da variável char estado
+printf ("Estado: \n"); 
+scanf("%s", estado1); // %s é o formato da variável char estado
 
-printf ("Código: %s \n", codigo1); // %s é o formato da variável char codigo
+printf ("Código: \n");
+scanf("%s", codigo1); // %s é o formato da variável char codigo
 
-printf ("Nome da cidade: %s \n", nomedacidade1); // %s é o formato da variável char nomedecidade
+printf ("Nome da cidade: \n");
+scanf("%s", nomedacidade1); // %s é o formato da variável char nomedecidade
 
-printf ("População: %d \n", populacao1); // %d é o formato da variável int populacao
+printf ("População: \n");
+scanf("%d", &populacao1); // %d é o formato da variável int populacao
 
-printf ("Área: %f \n", area1); // %f é o formato da variável float area
+printf ("Área: \n");
+scanf("%f", &area1); // %f é o formato da variável float area
 
-printf ("PIB: %f \n", pib1); // %f é o formato da variável float pib
+printf ("PIB: \n"); 
+scanf("%f", &pib1);  // %f é o formato da variável float pib
 
-printf ("Ponto turístico: %d \n", pontoturistico1); // %d é o formato da variável int pontoturistico
+printf ("Ponto turístico: \n");
+scanf("%d", &pontoturistico1); // %d é o formato da variável int pontoturistico
+
+densidadepopulacional1 = (float) populacao1 / area1;
+pibpercapita1 = pib1 / populacao1;
+
+printf ("Densidade populacional: %f \n", densidadepopulacional1);  // %f é o formato da variável float densidadepopulacional
+
+printf ("Pib per capita: %f \n", pibpercapita1);  // %f é o formato da variável pibpercapita
+
   // Área para exibição dos dados da cidade
   
- printf ("Carta 2: \n" );
+printf ("Carta 2: \n" );
+printf ("Estado: \n");
+scanf("%s", estado2); // %s é o formato da variável char estado
 
-printf ("Estado: %s \n", estado2); // %s é o formato da variável char estado
 
-printf ("Código: %s \n", codigo2); // %s é o formato da variável char codigo
+printf ("Código: \n");
+scanf("%s", codigo2); // %s é o formato da variável char codigo
 
-printf ("Nome da cidade: %s \n", nomedacidade2); // %s é o formato da variável char nomedecidade
+printf ("Nome da cidade: \n");
+scanf("%s", nomedacidade2); // %s é o formato da variável char nomedacidade
 
-printf ("População: %d \n", populacao2); // %d é o formato da variável int populacao
+printf ("População: \n");
+scanf("%d", &populacao2); // %d é o formato da variável int populacao
 
-printf ("Área: %f \n", area2); // %f é o formato da variável float area
+printf ("Área: \n");
+scanf("%f", &area2); // %f é o formato da variável float area
 
-printf ("PIB: %f \n", pib2); // %f é o formato da variável float pib
+printf ("PIB: \n");
+scanf("%f", &pib2); // %f é o formato da variável float pib
 
-printf ("Ponto turístico: %d \n", pontoturisco2); // %d é o formato da variável int pontoturistico
+printf ("Ponto turístico: \n");
+scanf("%d", &pontoturistico2); // %d é o formato da variável int pontoturistico
+
+densidadepopulacional2 = (float) populacao2 / area2;
+pibpercapita2 = pib2 / populacao2;
+
+printf ("Densidade populacional: %f \n", densidadepopulacional2); // %f é o formato da variável float densidadepopulacional
+
+printf ("PIB per capita: %f \n", pibpercapita2); // %f é o formato da variável pibpercapita
+
 
   // Área para exibição dos dados da cidade
 
